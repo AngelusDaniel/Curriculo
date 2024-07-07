916174dcb268eb152de31196533d26d8be949b9d
AngelusDaniel <danielangelus@yahoo.com.br>
2024-07-07 09:42:35 -0300

    site ver 1.2.2

--- script.js
+++ script.js
@@ -53,3 +53,35 @@ video.addEventListener('timeupdate', function(){
         BigInt.className = "play";
     }
 })
+
+class FormSubmit {
+    constructor(settings) {
+        this.settings = settings
+        this.form = document.querySelector(settings.form);
+        this.formButton = document.querySelector(settings.button)
+
+        if (this.form) {
+            this.url = this.form.getAttribute("action")
+        }
+    }
+
+    displaySucces() {
+        this.form.innerHTML = this.settings.success;
+    }
+
+    displaySucces() {
+        this.form.innerHTML = this.settings.error;
+    }
+
+    init() {
+        if (this.form) this.formButton.addEventListener ("click", () => this.displaySuccess())
+            return this;
+    }
+}
+
+const FormSubmit = new FormSubmit({
+    form: "[data-form]",
+    button: "[data-button]",
+    success: alert("Mensagem Enviada!"),
+    error: alert("Não foi possível enviar sua mensagem")
+});
